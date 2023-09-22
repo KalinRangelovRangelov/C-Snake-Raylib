a9dd296d3125f5f3d487139689fd86dbb7475314
Kalin Rangelov <kalin.rangelov@gmail.com>
2023-09-23 01:33:04 +0200

    Extracting variables, adding more colors for particles.

--- rayapp/particles.h
+++ rayapp/particles.h
@@ -3,9 +3,17 @@
 
 #include <raylib.h>
 
-// ChatGPT generated code
 
 #define MAX_PARTICLES 10
+#define PARTICLES_LIFETIME 5
+#define PARTICLES_SIZE 10
+#define PARTICLES_VELOCITY 10
+
+Color particle_colors[12] = {
+	GOLD, ORANGE, PINK, MAROON, RED, LIME, SKYBLUE, BLUE, PURPLE, VIOLET, YELLOW, MAGENTA
+};
+
+// ChatGPT generated code
 
 typedef struct {
 	Vector2 position;
@@ -31,9 +39,12 @@ void particles_spawn(Vector2 position, int numParticlesToSpawn) {
 	    for (int j = 0; j < MAX_PARTICLES; j++) {
 	        if (!particles[j].active) {
 	            // Initialize a new particle
-	            Vector2 velocity = (Vector2){ GetRandomValue(-10, 10), GetRandomValue(-10, 10) };
-	            Color color = RED; // Customize the color
-	            int lifetime = 10; // Adjust the lifetime as needed
+	            Vector2 velocity = (Vector2) { 
+					GetRandomValue(-PARTICLES_VELOCITY, PARTICLES_VELOCITY), 
+					GetRandomValue(-PARTICLES_VELOCITY, PARTICLES_VELOCITY) 
+				};
+	            Color color = particle_colors[rand() % 12];
+	            int lifetime = PARTICLES_LIFETIME; // Adjust the lifetime as needed
 	            particles_init(&particles[j], position, velocity, color, lifetime);
 	            break;
 	        }
@@ -59,7 +70,7 @@ void particles_draw() {
 	for (int i = 0; i < MAX_PARTICLES; i++) {
 	    if (particles[i].active) {
 	        //DrawPixelV(particles[i].position, particles[i].color);
-			DrawRectangle(particles[i].position.x, particles[i].position.y, 10, 10, particles[i].color);
+			DrawRectangle(particles[i].position.x, particles[i].position.y, PARTICLES_SIZE, PARTICLES_SIZE, particles[i].color);
 	    }
 	}
 }
